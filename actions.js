5793ae1a9e80d2639b387614a8e25b0eeacd9a28
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-13 14:34:59 +0200

    Add const - add comment

--- actions.js
+++ actions.js
@@ -1,5 +1,6 @@
 import uuid from "uuid";
 
+export const ADD_COMMENT = 'ADD_COMMENT';
 export const REMOVE_COMMENT = 'REMOVE_COMMENT';
 export const EDIT_COMMENT = 'EDIT_COMMENT';
 export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
